2fb9faff2eb852aa7c5f561bca70c3f70a63cf37
Ivan Nikolić <niksy5@gmail.com>
2016-12-04 16:32:25 +0100

    Upgrade package

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,50 @@
-var path = require('path');
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var plumber = require('gulp-plumber');
-var gutil = require('gulp-util');
-var debug = require('gulp-debug');
-var postcss = require('gulp-postcss');
-var nunjucks = require('gulp-nunjucks-render');
-var atImport = require('postcss-import');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var del = require('del');
-var ws = require('local-web-server');
-var opn = require('opn');
-var minimist = require('minimist');
-var globby = require('globby');
-var es = require('event-stream');
+/* globals process:false */
+/* eslint-disable no-process-env */
 
-var args = minimist(process.argv.slice(2), {
+'use strict';
+
+const path = require('path');
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const plumber = require('gulp-plumber');
+const gutil = require('gulp-util');
+const debug = require('gulp-debug');
+const nunjucks = require('gulp-nunjucks-render');
+const babayaga = require('@niksy/babayaga');
+const del = require('del');
+const ws = require('local-web-server');
+const opn = require('opn');
+const minimist = require('minimist');
+const globby = require('globby');
+const postcss = require('gulp-postcss');
+const atImport = require('postcss-import');
+
+const args = minimist(process.argv.slice(2), {
 	'default': {
 		watch: false,
 		port: 9000
 	}
 });
-var watch = args.watch;
-var port = args.port;
+const watch = args.watch;
+const port = args.port;
 
 function handleError ( msg ) {
 	gutil.log(gutil.colors.red(msg.message));
 	this.emit('end');
 }
 
-gulp.task('test:cleanup', function () {
+gulp.task('test:cleanup', () => {
 	return del([
 		'./test-dist'
 	]);
 });
 
-gulp.task('test:markup', ['test:cleanup'], function () {
+gulp.task('test:markup', ['test:cleanup'], () => {
 	function bundle () {
 		return gulp.src('./test/manual/suite/**/*.html')
+				.pipe(plumber(handleError))
 				.pipe(nunjucks())
+				.pipe(plumber.stop())
 				.pipe(gulp.dest('./test-dist'))
 				.pipe(debug({ title: 'Markup:' }));
 	}
@@ -53,7 +56,7 @@ gulp.task('test:markup', ['test:cleanup'], function () {
 
 gulp.task('test:style', ['test:cleanup'], function () {
 	function bundle () {
-		return gulp.src('./test/manual/suite/**/*.css')
+		return gulp.src(['./test/manual/suite/**/*.css', './*.css'])
 				.pipe(plumber(handleError))
 				.pipe(sourcemaps.init({
 					loadMaps: true
@@ -67,73 +70,64 @@ gulp.task('test:style', ['test:cleanup'], function () {
 				.pipe(debug({ title: 'Style:' }));
 	}
 	if ( watch ) {
-		gulp.watch(['./test/manual/suite/**/*.css'], bundle);
+		gulp.watch(['./test/manual/suite/**/*.css', './*.css'], bundle);
 	}
 	return bundle();
 });
 
-gulp.task('test:script', ['test:cleanup'], function ( done ) {
+gulp.task('test:script', ['test:cleanup'], () => {
 
-	globby(['./test/manual/suite/**/*.js'])
-		.then(function ( files ) {
-
-			function task ( file ) {
-
-				var b = browserify({
-					entries: [file],
-					debug: true,
-					cache: {},
-					packageCache: {}
-				});
-				if ( watch ) {
-					b.plugin(watchify);
-				}
-
-				function bundle () {
-					return b.bundle()
-						.on('error', handleError)
-						.pipe(plumber(handleError))
-						.pipe(source(path.basename(file)))
-						.pipe(buffer())
-						.pipe(sourcemaps.init({
-							loadMaps: true
-						}))
-						.pipe(sourcemaps.write())
-						.pipe(plumber.stop())
-						.pipe(gulp.dest(path.join('./test-dist', path.dirname(file).split(path.sep).pop())));
-				}
-
-				if ( watch ) {
-					b.on('update', function () {
-						bundle()
-							.pipe(debug({ title: 'Script:' }));
-					});
-					b.on('log', gutil.log);
-				}
-
-				return bundle();
-
-			}
-
-			if ( files.length ) {
-				es.merge(files.map(task))
-					.pipe(debug({ title: 'Script:' }))
-					.on('data', function () {})
-					.on('end', done);
-			} else {
-				done();
-			}
-
-			return files;
-
+	return globby(['./test/manual/suite/**/*.js'])
+		.then(( files ) => {
+			return files
+				.map(( file ) => {
+					const extname = path.extname(file);
+					const key = path.basename(file, extname);
+					const obj = {};
+					obj[`${path.dirname(file).split(path.sep).pop()}/${key}`] = file;
+					return obj;
+				})
+				.reduce(( prev, next ) => {
+					return Object.assign(prev, next);
+				}, {});
 		})
-		.catch(function ( err ) {
-			done(err);
+		.then(( entries ) => {
+
+			return babayaga({
+				entries: entries,
+				output: {
+					path: './test-dist'
+				},
+				dev: true,
+				watch: watch,
+				setupBundle: ( bundle ) => {
+					return bundle;
+				},
+				onAfterWrite: ( stream, isAsyncTask, isWatchUpdate ) => {
+					if ( isWatchUpdate ) {
+						return stream
+							.pipe(debug({ title: 'Script:' }));
+					}
+					return stream;
+				},
+				onStartBuild: ( stream, isAsyncTask ) => {
+					if ( isAsyncTask ) {
+						return stream
+							.pipe(debug({ title: 'Script:' }));
+					}
+					return stream
+						.pipe(debug({ title: 'Script:' }));
+				},
+				onError: ( err ) => {
+					gutil.log(gutil.colors.red(err.message));
+				}
+			}).build();
+
 		});
 
 });
 
-gulp.task('test:assets', ['test:cleanup'], function () {
+gulp.task('test:assets', ['test:cleanup'], () => {
 	function bundle () {
 		return gulp.src('./test/manual/assets/**/*')
 				.pipe(gulp.dest('./test-dist/assets'))
@@ -147,7 +141,7 @@ gulp.task('test:assets', ['test:cleanup'], function () {
 
 gulp.task('test:prepare', ['test:cleanup', 'test:markup', 'test:style', 'test:script', 'test:assets']);
 
-gulp.task('test:local:manual', ['test:prepare'], function () {
+gulp.task('test:local:manual', ['test:prepare'], () => {
 	if ( watch ) {
 		ws({
 			'static': {
@@ -157,6 +151,6 @@ gulp.task('test:local:manual', ['test:prepare'], function () {
 				path: './test-dist'
 			}
 		}).listen(port);
-		opn('http://localhost:' + port);
+		opn(`http://localhost:${port}`);
 	}
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,77 +1,87 @@
-/* eslint-disable no-process-env, no-process-exit, quote-props */
+/* globals process:false */
+/* eslint-disable no-process-env, no-process-exit, no-console */
 
-var http = require('http');
-var BrowserStackTunnel = require('browserstacktunnel-wrapper');
-var ws = require('local-web-server');
-var shutdown = require('http-shutdown');
-var execa = require('execa');
-var minimist = require('minimist');
-var server, tunnel;
+'use strict';
 
-var args = minimist(process.argv.slice(2), {
+const http = require('http');
+const ws = require('local-web-server');
+const shutdown = require('http-shutdown');
+const execa = require('execa');
+const minimist = require('minimist');
+let server, config;
+
+const args = minimist(process.argv.slice(2), {
 	'default': {
 		local: false,
 		verbose: false,
 		port: 9002
 	}
 });
-var local = args.local;
-var verbose = args.verbose;
-var port = args.port;
-
-var capabilities = [{
-	browser: 'Chrome',
-	os: 'Windows',
-	'os_version': '7',
-	project: 'rationalize.css',
-	build: 'Integration (WebdriverIO)',
-	name: 'Chrome',
-	browserName: 'Chrome',
-	'browserstack.local': 'true',
-	'browserstack.debug': 'true'
-}, {
-	browser: 'Firefox',
-	os: 'Windows',
-	'os_version': '7',
-	project: 'rationalize.css',
-	build: 'Integration (WebdriverIO)',
-	name: 'Firefox',
-	browserName: 'Firefox',
-	'browserstack.local': 'true',
-	'browserstack.debug': 'true'
-}, {
-	browser: 'IE',
-	'browser_version': '8',
-	os: 'Windows',
-	'os_version': '7',
-	project: 'rationalize.css',
-	build: 'Integration (WebdriverIO)',
-	name: 'IE8',
-	browserName: 'IE8',
-	'browserstack.local': 'true',
-	'browserstack.debug': 'true'
-}];
+const local = args.local;
+const verbose = args.verbose;
+const port = args.port;
+const dockerhost = 'dockerhost';
 
 if ( local ) {
-	capabilities = [{
-		browserName: 'chrome'
-	}];
+	config = {
+		baseUrl: `http://${dockerhost}:${port}`,
+		capabilities: [{
+			browserName: 'chrome'
+		}]
+	};
+} else {
+	config = {
+		baseUrl: `http://localhost:${port}`,
+		user: process.env.BROWSER_STACK_USERNAME,
+		key: process.env.BROWSER_STACK_ACCESS_KEY,
+		browserstackLocal: true,
+		services: ['browserstack'],
+		capabilities: [{
+			browser: 'Chrome',
+			os: 'Windows',
+			'os_version': '7',
+			project: 'rationalize-css',
+			build: 'Integration (WebdriverIO)',
+			name: 'Chrome',
+			browserName: 'Chrome',
+			'browserstack.local': 'true',
+			'browserstack.debug': 'true'
+		}, {
+			browser: 'Firefox',
+			os: 'Windows',
+			'os_version': '7',
+			project: 'rationalize-css',
+			build: 'Integration (WebdriverIO)',
+			name: 'Firefox',
+			browserName: 'Firefox',
+			'browserstack.local': 'true',
+			'browserstack.debug': 'true'
+		}, {
+			browser: 'IE',
+			'browser_version': '9',
+			os: 'Windows',
+			'os_version': '7',
+			project: 'rationalize-css',
+			build: 'Integration (WebdriverIO)',
+			name: 'IE9',
+			browserName: 'IE9',
+			'browserstack.local': 'true',
+			'browserstack.debug': 'true'
+		}]
+	};
 }
 
-exports.config = {
-	user: local ? null : process.env.BROWSER_STACK_USERNAME,
-	key: local ? null : process.env.BROWSER_STACK_ACCESS_KEY,
+module.exports.config = Object.assign({
 	specs: [
 		'./test/integration/**/*.js'
 	],
 	exclude: [],
 	maxInstances: 10,
-	capabilities: capabilities,
 	sync: false,
 	logLevel: verbose ? 'verbose' : 'silent',
 	coloredLogs: true,
 	screenshotPath: './errorShots/',
-	baseUrl: (local ? 'http://dockerhost:' : 'http://localhost:') + port,
+	screenshotOnReject: true,
 	waitforTimeout: 10000,
 	connectionRetryTimeout: 90000,
 	connectionRetryCount: 3,
@@ -82,7 +92,7 @@ exports.config = {
 	},
 	onPrepare: function () {
 
-		var startProcess = new Promise(function ( resolve, reject ) {
+		const startProcess = new Promise(( resolve, reject ) => {
 
 			server = shutdown(http.createServer(ws({
 				'static': {
@@ -96,44 +106,19 @@ exports.config = {
 				}
 			}).callback()));
 
-			console.log('Starting local web server on port ' + port + '…');
+			console.log(`Starting local web server on port ${port}…`);
 			server.listen(port);
 
-			if ( local ) {
-
-				console.log('Starting WebdriverIO…');
-				resolve();
-
-			} else {
-
-				tunnel = new BrowserStackTunnel({
-					key: process.env.BROWSER_STACK_ACCESS_KEY,
-					hosts: [{
-						name: 'localhost',
-						port: port
-					}]
-				});
-
-				console.log('Starting BrowserStack tunnel…');
-				tunnel.start(function ( err ) {
-					if ( err ) {
-						reject(err);
-						return;
-					}
-
-					console.log('Starting WebdriverIO…');
-					resolve();
-				});
-
-			}
+			console.log('Starting WebdriverIO…');
+			resolve();
 
 		});
 
 		return Promise.resolve()
-			.then(function () {
+			.then(() => {
 				if ( local ) {
-					return execa.shell('docker run --name=wdio --add-host="dockerhost:10.0.2.2" -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:2.53.0')
-						.then(function ( res ) {
+					return execa.shell(`docker run --name=wdio --add-host="${dockerhost}:10.0.2.2" -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:2.53.0`)
+						.then(( res ) => {
 							console.log(res.stderr);
 							console.log(res.stdout);
 							return startProcess;
@@ -141,7 +126,7 @@ exports.config = {
 				}
 				return startProcess;
 			})
-			.catch(function ( err ) {
+			.catch(( err ) => {
 				console.log(err);
 
 				console.log('Stopping local web server…');
@@ -154,38 +139,21 @@ exports.config = {
 
 	onComplete: function () {
 
-		var stopProcess = new Promise(function ( resolve, reject ) {
+		const stopProcess = new Promise(( resolve, reject ) => {
 
 			console.log('Stopping local web server…');
 			server.shutdown();
 
-			if ( local ) {
-
-				console.log('Stopping WebdriverIO…');
-				resolve();
-
-			} else {
-
-				console.log('Stopping BrowserStack tunnel…');
-				tunnel.stop(function ( err ) {
-					if ( err ) {
-						reject(err);
-						return;
-					}
-
-					console.log('Stopping WebdriverIO…');
-					resolve();
-				});
-
-			}
+			console.log('Stopping WebdriverIO…');
+			resolve();
 
 		});
 
 		return Promise.resolve()
-			.then(function () {
+			.then(() => {
 				if ( local ) {
 					return execa.shell('docker stop wdio && docker rm wdio')
-						.then(function ( res ) {
+						.then(( res ) => {
 							console.log(res.stdout);
 							console.log(res.stderr);
 							return stopProcess;
@@ -193,10 +161,10 @@ exports.config = {
 				}
 				return stopProcess;
 			})
-			.catch(function ( err ) {
+			.catch(( err ) => {
 				console.log(err);
 				process.exit(1);
 			});
 
 	}
-};
+}, config);
